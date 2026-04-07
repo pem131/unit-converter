0c2ca0d0ba4a9bd3fb1bd7f66e46e1b91f5f2f54
username <153721947+pem131@users.noreply.github.com>
2026-04-07 19:21:14 +0200

    Updated main

--- main.py
+++ main.py
@@ -2,62 +2,66 @@ import argparse
 import unit_conversion as uc
 from helper_functions import perform_conversion, print_supported_units
 
-# Instantiate a parser
-parser = argparse.ArgumentParser(description="A command line tool to convert US customary units to metric units.\
-    Provide a value and a unit to the command line and conversion will be done.")
+def main():
+    # Instantiate a parser
+    parser = argparse.ArgumentParser(description="A command line tool to convert US customary units to metric units.\
+        Provide a value and a unit to the command line and conversion will be done.")
 
-# Add arguments
-parser.add_argument("value", type=float, nargs="?", help="The value to be converted")
-parser.add_argument("unit", type=str, nargs="*", help="The unit to be converted")
+    # Add arguments
+    parser.add_argument("value", type=float, nargs="?", help="The value to be converted")
+    parser.add_argument("unit", type=str, nargs="*", help="The unit to be converted")
 
-# Add optional arguments
-parser.add_argument("-i", "--interactive", action="store_true", help="Run the tool in interactive mode")
-parser.add_argument("-s", "--show", action="store_true", help="Show supported conversions")
+    # Add optional arguments
+    parser.add_argument("-i", "--interactive", action="store_true", help="Run the tool in interactive mode")
+    parser.add_argument("-s", "--show", action="store_true", help="Show supported conversions")
 
-# Collect arguments
-args = parser.parse_args()
+    # Collect arguments
+    args = parser.parse_args()
 
-# Handle space arguments
-args.unit = " ".join(args.unit)
+    # Handle space arguments
+    args.unit = " ".join(args.unit)
 
-# List of conversion classes
-conversion_classes = [uc.Area, uc.Capacity, uc.Distance, uc.Mass, uc.Temperature, uc.Volume]
+    # List of conversion classes
+    conversion_classes = [uc.Area, uc.Capacity, uc.Distance, uc.Mass, uc.Temperature, uc.Volume]
 
-if args.interactive:
-    print("Entering interactive mode (type 'quit' to exit)")
-    # Create a while loop
-    while True:
-        cmd = input("Input a value and a unit (E.g., 3 feet) ").strip()
-        # Create an exit for the loop
-        if cmd.lower() == "quit":
-            break
-        
-        # Split the input
-        parts_input = cmd.split()
-        if len(parts_input) < 2:
-            print("Please provide a value and a unit")
-            continue
+    if args.interactive:
+        print("Entering interactive mode (type 'quit' to exit)")
+        # Create a while loop
+        while True:
+            cmd = input("Input a value and a unit (E.g., 3 feet) ").strip()
+            # Create an exit for the loop
+            if cmd.lower() == "quit":
+                break
+            
+            # Split the input
+            parts_input = cmd.split()
+            if len(parts_input) < 2:
+                print("Please provide a value and a unit")
+                continue
 
-        try:
-            # Collect the value
-            val = float(parts_input[0])
-            # Collect the unit
-            unit = " ".join(parts_input[1:])
-            # Perform the conversion
-            perform_conversion(value=val, unit=unit, conversion_classes=conversion_classes)
+            try:
+                # Collect the value
+                val = float(parts_input[0])
+                # Collect the unit
+                unit = " ".join(parts_input[1:])
+                # Perform the conversion
+                perform_conversion(value=val, unit=unit, conversion_classes=conversion_classes)
 
-        except ValueError as e:
-            # Only runs if float conversion fails
-            print(e)
-            continue
+            except ValueError as e:
+                # Only runs if float conversion fails
+                print(e)
+                continue
 
-elif args.show:
-    print_supported_units()
-        
-elif args.value is not None and args.unit is not None:
-    # Single command line usage
-    perform_conversion(value=args.value, unit=args.unit, conversion_classes=conversion_classes)
+    elif args.show:
+        print_supported_units()
+            
+    elif args.value is not None and args.unit is not None:
+        # Single command line usage
+        perform_conversion(value=args.value, unit=args.unit, conversion_classes=conversion_classes)
 
-else:
-    # Empty input
-    parser.print_help()
+    else:
+        # Empty input
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
